c1c20bc8deac9b974fb369e9ef786d48ced5e53b
Srdjan.Ranisavljevic <Srdjan.Ranisavljevic@endava.com>
2019-03-12 18:10:26 +0100

    PRoradilo

--- src/main/java/AppiumEx.java
+++ src/main/java/AppiumEx.java
@@ -1,23 +1,135 @@
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 import javax.mail.Message;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Date;
 
 public class AppiumEx {
 
-
+    private static AndroidDriver driver;
 
     public static void main(String[] args) {
 
+
+        DesiredCapabilities capabilities= new DesiredCapabilities();
+        capabilities.setCapability("deviceName","POCOPHONE_F1");
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("noReset", true);
+//        capabilities.setCapability("skipUnlock", true);
+        capabilities.setCapability("appPackage", "com.cocacola.app.cee.dev");
+        capabilities.setCapability("appActivity", "com.cocacola.stories.main.MainActivity");
+
+        try {
+            driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
+        // -------- LOGIN ---------//
+
+        try {
+            Thread.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        MobileElement log_in = (MobileElement) driver.findElementById("welcome_button_log_in");
+        log_in.click();
+
+
+        try {
+            Thread.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // ------- FIND LOGIN FIELD AND SEND KEYS - email address SUBMIT------------//
+
+        MobileElement enter_email = (MobileElement) driver.findElementById("auto_generated_edit_text");
+        enter_email.click();
+        enter_email.sendKeys("johnnycashccapp@gmail.com");
+
+        MobileElement submit_buton = (MobileElement) driver.findElementById("next_lottie_button");
+        submit_buton.click();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        MobileElement enter_code = (MobileElement) driver.findElementById("magic_link_button_enter_code");
+        enter_code.click();
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //OVDE IDE KOD ZA CHUPANJE MAILA
 
         String host = "pop.gmail.com";// change accordingly
         String mailStoreType = "pop3";
         String username = "johnnycashccapp@gmail.com";// change accordingly
         String password = "cocacolapb1";// change accordingly
+        String code = "";
+        CheckingMails cm = new CheckingMails();
+        try {
+           code = cm.writePart(cm.fetch(host, mailStoreType, username, password));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        MobileElement text_value = (MobileElement) driver.findElementById("text_value");
+        text_value.click();
+        text_value.sendKeys(code);
 
-        //Call method fetch
+        MobileElement text_confirm = (MobileElement) driver.findElementById("text_confirm");
+        text_confirm.click();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        MobileElement tutorial_skip = (MobileElement) driver.findElementById("tutorial_skip");
+        tutorial_skip.click();
+
+//        Date start = new java.util.Date(); // Date of the start of the script;
+//        System.out.println(start);
+//        Date mailDate = null; // date on which mail arrived. It should be later the start date.
+//        String from = "";
+//
+//        String host = "pop.gmail.com";// change accordingly
+//        String mailStoreType = "pop3";
+//        String username = "johnnycashccapp@gmail.com";// change accordingly
+//        String password = "cocacolapb1";// change accordingly
+//
+//
+//
+//
 //        CheckingMails cm = new CheckingMails();
-        String codeSnipetFromMail = CheckingMails.fetch(host, mailStoreType, username, password);
-        System.out.println("OVO JE CODE SNIPET: " + codeSnipetFromMail);
-
-    }
+//
+//        // Get mailDate
+////        try {
+////          mailDate = cm.retrieveSendDate(cm.fetch(host, mailStoreType, username,password));
+////
+////        } catch (Exception e) {
+////            e.printStackTrace();
+////        }
+////
+////        // get sender
+////        try {
+////            from = cm.getSender(cm.fetch(host, mailStoreType, username,password));
+////
+////        } catch (Exception e) {
+////            e.printStackTrace();
+////        }
+   }
 }
--- src/main/java/CheckingMails.java
+++ src/main/java/CheckingMails.java
@@ -10,8 +10,10 @@ import java.util.Properties;
 
 public class CheckingMails {
 
-    public static String fetch(String pop3Host, String storeType, String user,
+    public Message fetch(String pop3Host, String storeType, String user,
                              String password) {
+
+        Message returnedMessage = null;
         String cs = "";
         try {
             // create properties field
@@ -30,50 +32,37 @@ public class CheckingMails {
             // create the folder object and open it
             Folder emailFolder = store.getFolder("INBOX");
             emailFolder.open(Folder.READ_ONLY);
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(
-//                    System.in));
+
 
             // retrieve the messages from the folder in an array and print it
             Message[] messages = emailFolder.getMessages();
             System.out.println("messages.length---" + messages.length);
 
-            cs = writePart(messages[(messages.length - 1)]);
-//            for (int i = messages.length - 1; i > 0; i--) {
-//                Message message = messages[i];
-//                System.out.println("---------------------------------");
-//                writePart(message);
-//                break;
-//                String line = reader.readLine();
-//                if ("YES".equals(line)) {
-//                    message.writeTo(System.out);
-//                } else if ("QUIT".equals(line)) {
-//                    break;
-//                }
-//            }
+            returnedMessage =  messages[(messages.length-1)];
+//            cs = writePart(messages[(messages.length - 1)]);
+
 
             // close the store and folder objects
-            emailFolder.close(false);
-            store.close();
+//            emailFolder.close(false);
+//            store.close();
         } catch (NoSuchProviderException e) {
             e.printStackTrace();
         } catch (MessagingException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         }finally {
-            return cs;
+            return returnedMessage;
         }
     }
 
 
 
-//     This method checks for content-type
-////   based on which, it processes and
-////   fetches the content of the message
+             // This method checks for content-type based on which, it processes and fetches the content of the message
 
-    public static String writePart(Part p) throws Exception {
+    public String writePart(Part p) throws Exception {
         if (p instanceof Message)
             //Call methods writeEnvelope
             writeEnvelope((Message) p);
@@ -87,10 +76,9 @@ public class CheckingMails {
             Document docMultiPart = Jsoup.parse(strMultiPart);
             Element magicLinkURL = docMultiPart.select("a").first();
             String parsedLink = magicLinkURL.toString();
-            String codeSnippet = parsedLink.substring(60,92);
-//            System.out.println(codeSnippet);
+            String codeSnippet = parsedLink.substring(72,106);
             return codeSnippet;
-//                writePart(mp.getBodyPart(1));
+
 
         }
         else {
@@ -98,7 +86,7 @@ public class CheckingMails {
             Document doc = Jsoup.parse(html);
             Element magicLinkURL= doc.select("a").first();
             String parsedLink = magicLinkURL.toString();
-            String codeSnippet = parsedLink.substring(60,92);
+            String codeSnippet = parsedLink.substring(72,106);
 //            System.out.println(codeSnippet);
             return codeSnippet;
         }
@@ -109,6 +97,7 @@ public class CheckingMails {
 //      This method would print SENDDATE, FROM,TO and SUBJECT of the message
 
     public static void writeEnvelope(Message m) throws Exception {
+
 
         Address[] a;
 
@@ -134,8 +123,34 @@ public class CheckingMails {
             System.out.println("SUBJECT: " + m.getSubject());
     }
 
+    // This method retrieves sendDate for needed for checking the email with valid magic link
+
+    public Date retrieveSendDate (Message m) throws Exception {
+
+        Address[] a;
+
+        Date sendDate = m.getSentDate();
+        System.out.println(sendDate);
+        return sendDate;
+    }
+
+    public String getSender (Message m) throws Exception {
+        Address[] a;
+        String sender = "";
+        if ((a = m.getFrom()) != null) {
+            for (int j = 0; j < a.length; j++)
+                sender =  a[j].toString();
+        }
+        return sender;
+
+    }
+
 
 }
+
+
+
+
 
 //TREBA MI PRVO CLASSA KOJA CE DA PROVERAVA SEND TIME I SENDER
 // AKO SU SENDTIME > ScriptStart time && FROM = janrain@gmail.com) nastavi na cupanje maila
